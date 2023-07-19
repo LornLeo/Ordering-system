270ee9cebf95b3400826d50fde0ad60a02e2ee03
LornLeo <134342527+LornLeo@users.noreply.github.com>
2023-07-20 11:37:24 +1200

    Set up the initial menu item dictionaries list

--- main_page.py
+++ main_page.py
@@ -1,7 +1,12 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import math
-Menu_index=[{'Image_path': 'salmon-518032_1280.jpg', 'Name': 'Pizza', 'Price': '$12.99 ', 'Description': 'Savor the taste of our delectable pizza topped with the finest ingredients. Perfectly baked for your enjoyment.'},{'Image_path': 'pancake-1984716_1280.jpg', 'Name': 'Burger', 'Price': '$9.99 ', 'Description': 'Indulge in our mouth-watering burger made with premium ingredients. Juicy, flavorful, and satisfying.'},{'Image_path': 'asparagus-2169305_1280.jpg', 'Name': 'Steak', 'Price': '$153.99 ', 'Description': 'Experience culinary perfection with our prime steak. Tender, flavorful, and cooked to perfection.'},{'Image_path': 'salmon-518032_1280.jpg','Name': 'Sushi','Price': '$14.99','Description': 'Made with the finest seafood and expertly crafted for an unforgettable dinner'}]
+
+#Set up the initial menu item dictionaries list
+Menu_index=[{'Image_path': 'salmon-fish.jpg', 'Name': 'Grilled salmon fish', 'Price': '$12.99 ', 'Description': 'Tender, flavorful salmon fillet, grilled to perfection with secret herbs and spices.'},
+{'Image_path': 'pancake.jpg', 'Name': 'Fluffy Pancake Stack', 'Price': '$9.99 ', 'Description': 'Indulgent pancakes, caramelized edges, topped with syrup, cream, and seasonal fruits.'},
+{'Image_path': 'steak.jpg', 'Name': 'Juicy Sirloin Steak', 'Price': '$15.99 ', 'Description': 'Prime beef, seared crust, juicy tenderness, served with roasted vegetables and savory sauces.'}]
+
 page_number=1
 total_page_number=math.ceil(len(Menu_index)/3)
 window = Tk()
