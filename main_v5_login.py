6d42ebd8889cabd2da9b3b599cdafbe813c088de
LornLeo <134342527+LornLeo@users.noreply.github.com>
2023-07-30 23:51:33 +1200

    Update main_v5_login.py

--- main_v5_login.py
+++ main_v5_login.py
@@ -2,13 +2,14 @@ from tkinter import *
 from PIL import ImageTk, Image
 from csv import writer
 from tkinter import messagebox
+from tkinter import ttk
 import csv
 import subprocess
 
 def login():
     # Function to handle the login process.
-    if Username_ent.get() and Password_ent.get():
-        with open("user_database.csv", 'r', newline='') as file:
+    if username_ent.get() and password_ent.get():
+        with open("user_database.csv", 'r', newline = '') as file:
             csvreader = csv.reader(file)
             rows = list(csvreader)
             a = -1
@@ -16,9 +17,9 @@ def login():
             for row in rows:
                 a = a + 1
                 if row != []:
-                    if row[1] == Username_ent.get():
+                    if row[1] == username_ent.get():
                         error = False
-                        if row[2] == Password_ent.get():
+                        if row[2] == password_ent.get():
                             window.destroy()
                             update_value("user_database.csv", a, "Status", "online")
                             subprocess.run(['python', 'main_v5_menu.py'])
@@ -28,7 +29,7 @@ def login():
             if error == True:
                 messagebox.showerror("showerror", "Invalid username")          
     else:
-        if Username_ent.get():
+        if username_ent.get():
             messagebox.showerror("Error", "Please enter the password")
         else:
             messagebox.showerror("Error", "Please enter the Username")
@@ -48,48 +49,40 @@ def update_value(csv_file, row_index, column_name, new_value):
         with open(csv_file, 'w', newline='') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerows(rows)
-
+#GUI Setup
 window = Tk()
 window.geometry('850x525')
 window.title("Login")
 window.resizable(0, 0)
 window.configure(bg="white")
 
-Entry_frame = Frame(window,width=400,bg="white")
-Entry_frame.place(x=180,y=300)
+entry_frame = Frame(window,width=400,bg="white")
+entry_frame.place(x=180,y=300)
 
 image_path = "logo.png"
 food_image = Image.open(image_path)
 resized_image = food_image.resize((200, 200), Image.ANTIALIAS)
 image = ImageTk.PhotoImage(resized_image)
-Logo = Label(window, image=image, bg="white")
-Logo.place(x=325, y=30)
+logo = Label(window, image=image, bg="white")
+logo.place(x=325, y=30)
 
+name_lbl = Label(window,text="Food Shop",bg="white",font=('Rockwell 22'),fg="#fcc302",width=10)
+name_lbl.place(x=343,y=240)
 
-Name_lbl=Label(window,text="Food Shop",bg="white",font=('Rockwell 22'),fg="#fcc302",width=10)
-Name_lbl.place(x=343,y=240)
-Username_lbl=Label(Entry_frame,text="Username: ",bg="white",font=('Arial 17'))
-Username_lbl.grid(row=1,column=0,sticky=W)
-Username_ent= Entry(Entry_frame,bd=3,width=25,font=('Arial 19'))
-Username_ent.grid(row=1, column=1,columnspan=2,sticky=W)
-Password_lbl=Label(Entry_frame,text="Password: ",bg="white",font=('Arial 17'))
-Password_lbl.grid(row=2,column=0,sticky=W,pady=(23,0))
-Password_ent= Entry(Entry_frame,show="*",bd=3,width=25,font=('Arial 19'))
-Password_ent.grid(row=2, column=1,columnspan=2,sticky=W,pady=(23,0))
-signup_button = Button(Entry_frame, text="Sign up", command=sign_up,width=18,font=('serif 13'),fg="white",bg="black")
+username_lbl = Label(entry_frame,text="Username: ",bg="white",font=('Arial 17'))
+username_lbl.grid(row=1,column=0,sticky=W)
+username_ent = Entry(entry_frame,bd=3,width=25,font=('Arial 19'))
+username_ent.grid(row=1, column=1,columnspan=2,sticky=W)
+
+password_lbl = Label(entry_frame,text="Password: ",bg="white",font=('Arial 17'))
+password_lbl.grid(row=2,column=0,sticky=W,pady=(23,0))
+password_ent = Entry(entry_frame,show="*",bd=3,width=25,font=('Arial 19'))
+password_ent.grid(row=2, column=1,columnspan=2,sticky=W,pady=(23,0))
+
+signup_button = Button(entry_frame, text="Sign up", command=sign_up,width=18,font=('serif 13'),fg="white",bg="black")
 signup_button.grid(row=4,column=1,pady=5)
-Login_button = Button(Entry_frame, text="Login", command=login,width=18,font=('serif 13'),fg="white",bg="black")
-Login_button.grid(row=4,column=2,pady=5)
-try:
-    with open("user_database.csv", 'r') as file:
-        csvreader = csv.reader(file)
-        b=0
-        for row in csvreader:
-            b=b+1
-            if b==1:
-                pass
-            else:
-                update_value("user_database.csv", (b-1), "Status", "offline")
-except:
-    pass
+
+login_button = Button(entry_frame, text="Login", command=login,width=18,font=('serif 13'),fg="white",bg="black")
+login_button.grid(row=4,column=2,pady=5)
+
 window.mainloop()
